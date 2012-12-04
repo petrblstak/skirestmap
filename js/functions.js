4af4c9c3a3153c6c2789fd532e15aae5a545f118
Sungbo Kang <sungbokang@gmail.com>
2012-12-04 16:13:21 +0900

    Revert "merging and conflict solving"
    This reverts commit 8966986b39b4aff5cd60a837c993daceb67c5c16, reversing
    changes made to e0c7315f9ab515c976131ce3debecd87882b9793.

--- js/functions.js
+++ js/functions.js
@@ -1,64 +1,9 @@
 // main vars
 // funtions
-
-//global variables
-var list = new Object(); // actual restaurants list
-var list_ref = new Array(); // reference of 'list' variable
-
-function storeTypes()
-{
+function storeTypes(){
     
 }
 
 function loadDetail(number){
     
 }
-
-function temp()
-{
-    alert(window.list.restaurant[0].name);
-} // temp
-
-function clone (obj, deep) { 
-  var objectClone = new obj.constructor(); 
-  for (var property in obj) 
-    if (!deep) 
-      objectClone[property] = obj[property]; 
-    else if (typeof obj[property] == 'object') 
-      objectClone[property] = obj[property].clone(deep); 
-    else 
-      objectClone[property] = obj[property]; 
-  return objectClone; 
-} // clone
-
-function loadListJSON()
-{
-    $.getJSON('doc/list.json', function(data)
-    {
-        window.list = clone(data, false);
-        
-    });
-   
-    //var arrayLength = list.restaurant.length;
-    
-    for(var i = 0; i < list.restaurant.length; i++)
-        list_ref[i] = i+1;
-    
-} // loadListJson
-
-function sortListAlphabetically()
-{
-    for (var i = 0; i < list.restaurant.length; i++)
-        {
-            for(var j = i+1; j < list.restaurant.length; j++)
-                {
-                    if(list.restaurant[list_ref[i]].name > list.restaurant[list_ref[j]].name)
-                        {
-                            var temp;
-                            temp = list_ref[i];
-                            list_ref[i] = list_ref[j];
-                            list_ref[j] = temp;
-                        }
-                }
-        }
-} // sortListAlphabetically
